43c12bfc85f505c881e78f9cb882d46e963a5755
Crysjurado20 <cjurado5795@uta.edu.ec>
2026-06-05 22:00:36 -0500

    Add environment files and update gitignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5132/api',
+  institutionEmailDomain: 'uta.edu.ec',
+  firebaseConfig: {
+    apiKey: "AIzaSyB8eELUMGPKrccEIgIipGw23ijL9kgnmx4",
+    authDomain: "u-ride-b1089.firebaseapp.com",
+    databaseURL: "https://u-ride-b1089-default-rtdb.firebaseio.com",
+    projectId: "u-ride-b1089",
+    storageBucket: "u-ride-b1089.firebasestorage.app",
+    messagingSenderId: "958685585973",
+    appId: "1:958685585973:web:b9ee81b588704da012877c"
+  }
+};
